9a8395d0ff91a612d64a513c6077439cbdddf4e0
jrmeen <59030143@kmitl.ac.th>
2017-09-21 22:51:11 +0700

    add repo22

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -6,11 +6,11 @@ class Program
     static void Main()
     {
 
+
         double d = 5e9d;
         Console.WriteLine(d);
-        int i = (int)d;
+        int i = checked((int)d);
         Console.WriteLine(i);
-
 
 
     }
