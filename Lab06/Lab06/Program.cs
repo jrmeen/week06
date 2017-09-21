b1d54c7f30053596d49dbe515d8e7cf80902990f
jrmeen <59030143@kmitl.ac.th>
2017-09-21 22:33:06 +0700

    add repo18

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -6,13 +6,12 @@ class Program
     static void Main()
     {
 
+        int myInt = 5;
+        Console.WriteLine(myInt);
+        long myLong = myInt;
+        Console.WriteLine(myLong);
+        Console.WriteLine(myLong + myInt);
 
-        string hello = "Hello";
-        string world = "World";
-        string helloWorld = hello + world;
-        Console.WriteLine(helloWorld);
-        string helloWorld555 = helloWorld + " " + 555;
-        Console.WriteLine(helloWorld555);
 
 
     }
